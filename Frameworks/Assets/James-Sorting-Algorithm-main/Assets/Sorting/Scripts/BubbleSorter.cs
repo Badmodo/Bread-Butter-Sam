9f9bda4d37be7d8f1440e8713f35802c64af495e
Sam <samleitch75@gmail.com>
2021-03-08 11:23:25 +1100

    Update ini

---
+++ Frameworks/Assets/James-Sorting-Algorithm-main/Assets/Sorting/Scripts/BubbleSorter.cs
@@ -0,0 +1,34 @@
+﻿using Sorting;
+using System.Collections;
+public class BubbleSorter : BaseSorter
+{
+    protected override IEnumerator Sort()
+    {
+        int nodeCount = nodes.Length;
+        Node tempNode;
+
+        // Loop over the array 'length-squared' times.
+        for (int i = 0; i <= nodeCount - 2; i++)
+        {
+            for (int j = 0; j <= nodeCount - 2; j++)
+            {
+                // Is this number greater than the one after it?
+                if (nodes[j].Index > nodes[j + 1].Index)
+                {
+                    // It is, so swap the elements.
+                    // Store the next element in a temp variable.
+                    tempNode = nodes[j + 1];
+                    // Set the next element to the current one.
+                    nodes[j + 1] = nodes[j];
+                    // Set this element to the temp one.
+                    nodes[j] = tempNode;
+
+                    // THIS SHIT IS JUST AESTHETIC
+                    StartFrame(j, j + 1);
+                    yield return null;
+                    EndFrame(j, j + 1);
+                }
+            }
+        }
+    }
+}
---
+++ Frameworks/Assets/James-Sorting-Algorithm-main/Assets/Sorting/Scripts/RadixSorter.cs
@@ -0,0 +1,34 @@
+﻿using Sorting;
+using System.Collections;
+using Array = System.Array;
+public class RadixSorter : BaseSorter
+{
+    protected override IEnumerator Sort()
+    {
+        int nodeCount = nodes.Length;
+        Node[] temp = new Node[nodes.Length];
+        int j, i;
+        for (int shift = 31; shift > -1; --shift)
+        {
+            // Set j to 0.
+            j = 0;
+
+            // Loop though the whole array.
+            for (i = 0; i < nodeCount; ++i)
+            {
+                // Determine if the bit-shifted variable is above 0.
+                bool move = (nodes[i].Index << shift) >= 0;
+                if (shift == 0 ? !move : move) nodes[i - j] = nodes[i];
+                else temp[j++] = nodes[i];
+            }
+
+            // Copy the data to the temp array.
+            Array.Copy(temp, 0, nodes, nodes.Length - j, j);
+
+            // James is done for the day...
+            StartFrame(0, 1);
+            yield return null;
+            EndFrame(0, 1);
+        }
+    }
+}
